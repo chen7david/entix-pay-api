b658162bc7160c36bca44a812c84239e64cf1bfb
David M. Chen <chen7david@me.com>
2023-09-18 09:23:11 -0300

    created: migrations

---
+++ src/database/migrations/20230918115527_create_permissions.ts
@@ -0,0 +1,19 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('permissions', (table) => {
+        table.increments('id').primary()
+        table.string('name').unique().notNullable()
+        table.string('short_description').notNullable()
+        table.text('description')
+        table.boolean('is_deleted')
+        table.timestamps(true, true)
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable('permissions')
+}
+
