c0f91450e0ec971e1e340cdf0982d35a6235a5dc
David M. Chen <chen7david@me.com>
2023-11-13 09:09:48 -0300

    added soft delete functionality

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,18 @@ const configPath = path.join(__dirname,`../.env.${nodeEnv}`)
 
 dotenv.config({ path: configPath})
 
-db('users').then(console.log)
+// db('users').then(console.log)
 
 
 
 const test = async () => {
-    await UserRepository.create<TUserEntity>({
-        username: 'lele3',
-        email: 'lele@mail.com',
-        password: '234242',
-        is_deleted: false
-    })
+    // await UserRepository.create<TUserEntity>({
+    //     username: 'lele34',
+    //     email: 'lele@mail.com',
+    //     password: '234242',
+    //     is_deleted: false
+    // })
+    await UserRepository.unDelete(2)
     const data = await UserRepository.getAll()
     console.log(data)
 }
--- src/repository/base.repository.ts
+++ src/repository/base.repository.ts
@@ -10,4 +10,22 @@ export class BaseRepository {
     static async create<T> (data: T) {
         return db(this.tableName).insert(data)
     }
+
+    static async delete<T> (id: number | string) {
+        return db(this.tableName).delete().where('id', id)
+    }
+
+    static async update<T>(id: number | string, data: T) {
+        const item = await db(this.tableName).where('id', id).first() as T
+        if(!item) throw(new Error(`could not find ${this.tableName} with id ${id}`))
+        return db(this.tableName).update(data).where('id', id)
+    }
+
+    static async softDelete<T extends { is_deleted?: boolean }>(id: number | string) {
+        return this.update(id, {is_deleted: true})
+    }
+
+    static async unDelete<T extends { is_deleted?: boolean }>(id: number | string) {
+        return this.update(id, {is_deleted: false})
+    }
 }
